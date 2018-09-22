0a023864630350f24e980bebd6b66da57ac7a43a
Bobby Rohweder <BobbyRohweder@gmail.com>
2018-09-22 16:53:37 -0700

    Initial Commit

---
+++ NintendoSwitchEshop/NintendoSwitchEshop.h
@@ -0,0 +1,19 @@
+//
+//  NintendoSwitchEshop.h
+//  NintendoSwitchEshop
+//
+//  Created by Bobby Rohweder on 9/22/18.
+//  Copyright © 2018 bobbyrohweder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NintendoSwitchEshop.
+FOUNDATION_EXPORT double NintendoSwitchEshopVersionNumber;
+
+//! Project version string for NintendoSwitchEshop.
+FOUNDATION_EXPORT const unsigned char NintendoSwitchEshopVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NintendoSwitchEshop/PublicHeader.h>
+
+
